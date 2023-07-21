b449f3f6a82dfa3eb8a1b2080e854c3d023ea98f
arfat132 <arfatbegum90@gmail.com>
2023-07-21 19:12:34 +0300

    [Done] add and edit book page

--- src/pages/EditBook.tsx
+++ src/pages/EditBook.tsx
@@ -1,10 +1,15 @@
+import EditBookForm from "@/components/EditBookForm";
+import Navbar from "@/layouts/Navbar";
 
 export default function EditBook() {
-    return (
-      <>
-        <p>Edit Book</p>
-      </>
-    )
-  }
+  return (
+    <>
+      <Navbar />
+      <div className="flex items-center justify-between w-full md:max-w-7xl h-full mx-auto">
+        <EditBookForm />
+        <img src="https://media.istockphoto.com/id/507876702/vector/cute-girl-in-pink-is-sitting-and-reading-a-book.jpg?s=612x612&w=0&k=20&c=g27LKHgByWKzAFcb4gHHnrteel2ZSnIpTDmyfqcx0L8=" />
+      </div>
+    </>
+  )
+}
 
-  
