b30de4d38b1dca79b5c849c0b703631361f2b9ea
arfat132 <arfatbegum90@gmail.com>
2023-07-21 20:26:11 +0300

    [Done] wishlist page

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,3 +1,5 @@
+import { Heart } from "lucide-react";
+
 export default function BookCard() {
     return (
         <div className="p-4 sm:mb-0 mb-6 overflow-hidden border border-gray-200">
@@ -7,7 +9,10 @@ export default function BookCard() {
             <h2 className="text-xl font-bold text-primary title-font mt-5">To Kill a Mockingbird</h2>
             <p className='text-[#37be4e] font-medium'> Harper Lee</p>
             <p className="text-base leading-relaxed mt-1">Classic Fiction</p>
-            <p className="text-base leading-relaxed mt-1">Publication Date: 1960</p>
+            <div className="flex justify-between items-center">
+                <p className="text-base leading-relaxed mt-1">Publication Date: 1960</p>
+                <Heart className='text-[#37be4e]' />
+            </div>
         </div>
     );
 }
--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -1,10 +1,23 @@
+import BookCard from "@/components/BookCard";
+import Navbar from "@/layouts/Navbar";
 
 export default function Wishlist() {
-    return (
-      <>
-        <p>Wishlist</p>
-      </>
-    )
-  }
+  return (
+    <>
+      <Navbar />
+      <div className="py-20 max-w-7xl mx-auto">
+        <h1 className="text-xl font-black text-primary uppercase mb-4" >
+          Wishlist
+        </h1>
+        <div className="grid grid-cols-5 gap-4">
+          <BookCard />
+          <BookCard />
+          <BookCard />
+          <BookCard />
+          <BookCard />
+        </div>
+      </div>
+    </>
+  )
+}
 
-  
