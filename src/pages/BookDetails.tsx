66fd19bbb1f097264b1d253a95d28825f873e5fa
arfat132 <arfatbegum90@gmail.com>
2023-07-25 04:17:31 +0300

    [done] bookdetails functionality

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -1,31 +1,114 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import BookReview from "@/components/BookReview";
 import { Button } from "@/components/ui/button";
 import Navbar from "@/layouts/Navbar";
-import { Link } from "react-router-dom";
+import Loader from "@/components/Loader";
+import { useDeleteBookMutation, useSingleBookQuery } from "@/redux/features/book/bookSlice";
+import { useAddToReadingListMutation, useAddToWishListMutation } from "@/redux/user/userApi";
+import { AiOutlineHeart } from "react-icons/ai";
+import { BiBookReader } from "react-icons/bi";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function BookDetails() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [addToWishList] = useAddToWishListMutation();
+  const [addToReadingList] = useAddToReadingListMutation();
+  const [deleteBook] = useDeleteBookMutation();
+
+  const storedAuthData = localStorage.getItem('auth');
+  const authData = storedAuthData ? JSON.parse(storedAuthData) : null;
+
+  const user = authData ? authData.user : null;
+  const { data, isLoading } = useSingleBookQuery(id);
+
+  const book = data?.data;
+  //const { _id, image, title, author, genre, publicationDate } = book;
+
+  let authorEmail = false;
+  if (user != null) {
+    if (user?.id === book?.authorEmail) {
+      authorEmail = true;
+    }
+  }
+
+  const handleAddToWishlist = async () => {
+    const response: any = await addToWishList({ id: book?._id });
+    if (response.error) {
+      toast.error(response.error.data.errorMessages[0].message);
+    } else {
+      toast.success(response.data.message);
+    }
+    console.log(response);
+  };
+
+  const handleAddToReadingList = async () => {
+    const response: any = await addToReadingList({ id: book?._id });
+    if (response.error) {
+      toast.error(response.error.data.errorMessages[0].message);
+    } else {
+      toast.success(response.data.message);
+    }
+  };
+
+  const handleDeleteBook = async () => {
+    const confirmDelete = window.confirm(
+      'Are you sure you want to delete this book?'
+    );
+    if (confirmDelete) {
+      const response: any = await deleteBook({ id: book?._id });
+      if (response.error) {
+        toast.error(response.error.data.errorMessages[0].message);
+      } else {
+        toast.success(response.data.message);
+        navigate('/all-books');
+      }
+    }
+  }
+
   return (
     <>
       <Navbar />
-
-      <div className="pt-28 pb-8">
-        <div className="max-w-7xl mx-auto flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-          <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://m.media-amazon.com/images/I/71HbYElfY0L._AC_UF1000,1000_QL80_.jpg" alt="" />
-          <div className="flex flex-col justify-between p-4 leading-normal">
-            <h2 className="text-xl font-bold text-primary title-font mt-5">To Kill a Mockingbird</h2>
-            <p className='text-[#37be4e] font-medium'> Harper Lee</p>
-            <p className="text-base leading-relaxed mt-1">Classic Fiction</p>
-            <p className="text-base leading-relaxed mt-1">Publication Date: 1960</p>
-            <div className="flex mt-2">
-              <Button className="bg-[#37be4e] uppercase text-sm font-semibold w-24 h-9 rounded-md px-3 mr-2">
-                <Link to="/edit-book">Edit</Link>
-              </Button>
-              <Button className="bg-[#37be4e] uppercase text-sm font-semibold w-24 h-9 rounded-md px-3">Delete </Button>
-            </div>
-          </div>
+      {isLoading ? (
+        <div className="flex justify-center h-[100vh]">
+          <Loader />
         </div>
-      </div >
-      <BookReview />
+      ) : (
+        <>
+          <div className="pt-28 pb-8">
+            <div className="relative max-w-7xl mx-auto flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={book?.image} alt="" />
+              <div className="flex flex-col justify-between p-4 leading-normal">
+                <h2 className="text-xl font-bold text-primary title-font mt-5">{book?.title}</h2>
+                <p className='text-[#37be4e] font-medium'> {book?.author}</p>
+                <p className="text-base leading-relaxed mt-1">{book?.genre}</p>
+                <p className="text-base leading-relaxed mt-1">Publication Date: {book?.publicationDate}</p>
+                {authorEmail && (
+                  <div className="flex mt-2">
+                    <Button className="bg-[#37be4e] uppercase text-sm font-semibold w-24 h-9 rounded-md px-3 mr-2">
+                      <Link to={`/edit-book/${book?._id}`}>Edit</Link>
+                    </Button>
+                    <Button onClick={handleDeleteBook} className="bg-[#37be4e] uppercase text-sm font-semibold w-24 h-9 rounded-md px-3">Delete </Button>
+                  </div>
+                )}
+                <div className="absolute top-5 right-5">
+                  <AiOutlineHeart
+                    onClick={handleAddToWishlist}
+                    className='text-[#37be4e] text-3xl bg-white border border-gray-200 shadow-sm mb-2 p-1 rounded' />
+                  <BiBookReader
+                    onClick={handleAddToReadingList}
+                    className='text-[#37be4e] text-3xl bg-white border border-gray-200 shadow-sm mb-2 p-1 rounded' />
+                </div>
+              </div>
+            </div >
+          </div >
+          <BookReview />
+          <ToastContainer />
+        </>
+      )
+      }
     </>
   )
 }
